559d2e9c6579d7164c804f4661b82250689a6cc7
Aaron Hu <aaron@ip-192-168-1-30.us-west-2.compute.internal>
2021-05-27 13:57:43 -0700

    create code to delete file and log all interactions

--- script.py
+++ script.py
@@ -3,18 +3,27 @@ from api_keys import *
 import requests
 import csv
 
-
 count = 0
 
-org_ids = [33107958, 3107957]
+org_ids = [265066382,1515260]
+
+log_file = open("log.csv", "w")
+log_file.write("org_id, status code, notes")
+log_file.write("\n")
 
 for each in org_ids:
   response = requests.delete("https://api.affinity.co/organizations/" + str(each), auth=('', api_key))
-  print("Status code: " + str(response.status_code))
-  print(response.text.encode('utf8'))
 
   if response.status_code == 200:
     count = count + 1
-    print("Number of notes deleted - " + str(count))
+    print(str(response.json()))
+    log_file.write(str(each) + ", " + str(response.status_code) + ", " + str(response.json()))
+    log_file.write("\n")
   else:
-    print("It failed.")
+  	string_reponse = ''
+  	log_file.write(str(each) + ", " + str(response.status_code) + ", " + response.json()[0])
+  	log_file.write("\n")
+
+log_file.write("\n")
+log_file.write("Number of successful organizations removed: " + str(count))
+log_file.close()
