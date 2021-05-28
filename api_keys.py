9415105abc48b846a002f52dd460550593a9c700
Aaron Hu <aaron@ip-192-168-1-30.us-west-2.compute.internal>
2021-05-27 19:51:02 -0700

    added current count

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,5 @@
 # Customer's API key
-api_key = 'Xh565C3RFgjI0ubln9KpzfEzk-ML1c_DWKTxpz8xZDk'
+# api_key = 'Xh565C3RFgjI0ubln9KpzfEzk-ML1c_DWKTxpz8xZDk'
+
+# Tesing api key
+api_key = "OalyOEk3YNr2_NpcBcVD9662L8fTyJ8-Wrpdnzde4Eg"
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
-from api_keys import *
-from orgs import *
-
 import requests
 import csv
 
-count = 0
+from api_keys import *
+from orgs import *
 
+
+total_count = len(org_ids)
+current_count = 0
+corrected_count = 0
 
 log_file = open("log.csv", "w")
 log_file.write("Organization ID, Status code, Note")
@@ -13,10 +15,11 @@ log_file.write("\n")
 
 for each in org_ids:
   response = requests.delete("https://api.affinity.co/organizations/" + str(each), auth=('', api_key))
+  current_count = current_count + 1
 
   if response.status_code == 200:
-    count = count + 1
-    print("Working on " + str(each) + " - "+ str(response.json()))
+    corrected_count = corrected_count + 1
+    print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - "+ str(response.json()))
     log_file.write(str(each) + ", " + str(response.status_code) + ", " + str(response.json()))
     log_file.write("\n")
   elif response.status_code == 429:
@@ -29,10 +32,10 @@ for each in org_ids:
   	print("Working on " + str(each) + " - "+ str(response.json()))
   	break
   else:
-  	print("Working on " + str(each) + " - "+ str(response.json()))
+  	print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - "+ str(response.json()))
   	log_file.write(str(each) + ", " + str(response.status_code) + ", " + response.json()[0])
   	log_file.write("\n")
 
 log_file.write("\n")
-log_file.write("Number of successful organizations removed: " + str(count))
+log_file.write("Number of successful organizations removed: " + str(corrected_count))
 log_file.close()
