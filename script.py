19894827956b3a981f106aac59ab21da78184494
Aaron Hu <aaron@ip-192-168-1-30.us-west-2.compute.internal>
2021-05-27 18:48:29 -0700

    add elif on 429, 500, 503 status. tested script, everything is working as expected

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 from api_keys import *
+from orgs import *
 
 import requests
 import csv
 
 count = 0
 
-org_ids = [265066382,1515260]
 
 log_file = open("log.csv", "w")
-log_file.write("org_id, status code, notes")
+log_file.write("Organization ID, Status code, Note")
 log_file.write("\n")
 
 for each in org_ids:
@@ -16,11 +16,20 @@ for each in org_ids:
 
   if response.status_code == 200:
     count = count + 1
-    print(str(response.json()))
+    print("Working on " + str(each) + " - "+ str(response.json()))
     log_file.write(str(each) + ", " + str(response.status_code) + ", " + str(response.json()))
     log_file.write("\n")
+  elif response.status_code == 429:
+  	print("Working on " + str(each) + " - "+ str(response.json()))
+  	break
+  elif response.status_code == 500:
+  	print("Working on " + str(each) + " - "+ str(response.json()))
+  	break
+  elif response.status_code == 503:
+  	print("Working on " + str(each) + " - "+ str(response.json()))
+  	break
   else:
-  	string_reponse = ''
+  	print("Working on " + str(each) + " - "+ str(response.json()))
   	log_file.write(str(each) + ", " + str(response.status_code) + ", " + response.json()[0])
   	log_file.write("\n")
 
