b0b8173f54b8d988552fab70eb375a9bac131d06
Aaron Hu <aaron1515@gmail.com>
2021-09-01 15:30:10 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # Customer's API key
-# api_key = 'Xh565C3RFgjI0ubln9KpzfEzk-ML1c_DWKTxpz8xZDk'
+# api_key = 'API KEY'
 
 # Tesing api key
-api_key = "OalyOEk3YNr2_NpcBcVD9662L8fTyJ8-Wrpdnzde4Eg"
+api_key = "API KEY"
